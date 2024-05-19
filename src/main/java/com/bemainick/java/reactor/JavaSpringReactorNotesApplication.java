68d08bc9e947daa4f885098333af5a5b67bfa117
erick11 <erick.eduardo.ee7@gmail.com>
2024-05-19 11:23:02 -0500

    chore: basic structure API

---
+++ src/main/java/com/bemainick/java/reactor/JavaSpringReactorNotesApplication.java
@@ -0,0 +1,13 @@
+package com.bemainick.java.reactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringReactorNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringReactorNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bemainick/java/reactor/JavaSpringReactorNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bemainick.java.reactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringReactorNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
